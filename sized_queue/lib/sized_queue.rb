add10d188d77da619fa226f0ab46b29d843155bf
Lin Jen-Shin <godfat@godfat.org>
2009-11-27 00:44:09 +0800

    [sized_queue.rb] done

--- sized_queue/lib/sized_queue.rb
+++ sized_queue/lib/sized_queue.rb
@@ -1,3 +1,19 @@
 
 class SizedQueue
+  include Enumerable
+  attr_accessor :size
+  attr_writer :data
+
+  def initialize size; @size = size      ;end
+  def each     &block; data.each(&block) ;end
+  def first          ; data.first        ;end
+  def data           ; @data ||= []      ;end
+  def == lhs         ; data == lhs.data  ;end
+  # should be more efficient than Enumerable's to_a?
+  def to_a           ; data.dup          ;end
+
+  def enqueue *args
+    a = args.reverse.take(size)
+    self.data = a + (a.size == size ? [] : data[0, size - a.size])
+  end
 end
--- sized_queue/test/test_sized_queue.rb
+++ sized_queue/test/test_sized_queue.rb
@@ -10,9 +10,36 @@ TestCase = begin
            end
 
 require 'sized_queue'
+require 'yaml'
 
 class SizedQueueTest < TestCase
   def test_sized_queue
-    assert true
+    q = SizedQueue.new(5)
+
+    q.enqueue :foo
+    assert_equal :foo, q.first
+
+    q.enqueue :bar
+    assert_equal :bar, q.first
+
+    assert_equal [:bar, :foo], q.to_a
+
+    q.enqueue(*[:a, :b, :c, :d])
+
+    data = [:d, :c, :b, :a, :bar]
+    assert_equal data, q.to_a
+    q.each_with_index{ |i, index|
+      assert_equal data[index], i
+    }
+
+    q.enqueue(*(0..9).to_a)
+    assert_equal [9, 8, 7, 6, 5], q.to_a
+  end
+
+  def test_yaml
+    q = SizedQueue.new(29)
+    q.enqueue(*(0..30).to_a)
+    assert_equal q, YAML.load(YAML.dump(q))
+    assert_equal q.data, YAML.load(YAML.dump(q)).data
   end
 end
