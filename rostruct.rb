494d67569606b7895aa212df3627a3254e5619c8
Lin Jen-Shin <godfat@godfat.org>
2013-12-09 17:36:19 +0800

    rostruct.rb: should also handle array values

--- rostruct.rb
+++ rostruct.rb
@@ -4,14 +4,20 @@ require 'ostruct'
 class RecOpenStruct < OpenStruct
   def initialize hash
     super(hash.inject({}){ |r, (k, v)|
-      r[k] = case v
-             when Hash
-               RecOpenStruct.new(v)
-             else
-               v
-             end
+      r[k] = self.class.convert(v)
       r
     })
+  end
+
+  def self.convert obj
+    case obj
+    when Hash
+      RecOpenStruct.new(obj)
+    when Array
+      obj.map{ |v| convert(v) }
+    else
+      obj
+    end
   end
 end
 
@@ -20,10 +26,18 @@ if $PROGRAM_NAME == __FILE__
   Bacon.summary_on_exit
   describe RecOpenStruct do
     should 'initialize' do
-      ros = RecOpenStruct.new(:a => :b, :c => {:d => :e})
-      ros.a  .should.eql     :b
-      ros.c  .should.kind_of RecOpenStruct
-      ros.c.d.should.eql     :e
+      ros = RecOpenStruct.new(:a => :b, :c => {:d => :e},
+                              :f => [:g, {:h => :i}, {:j => {:k => :h}}])
+      ros.a       .should.eql     :b
+      ros.c       .should.kind_of RecOpenStruct
+      ros.c.d     .should.eql     :e
+      ros.f       .should.kind_of Array
+      ros.f[0]    .should.eql     :g
+      ros.f[1]    .should.kind_of RecOpenStruct
+      ros.f[1].h  .should.eql     :i
+      ros.f[2]    .should.kind_of RecOpenStruct
+      ros.f[2].j  .should.kind_of RecOpenStruct
+      ros.f[2].j.k.should.eql     :h
     end
   end
 end
